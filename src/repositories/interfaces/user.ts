102d1ffac5731fb61777c32f99b3f847110604fb
Ashish Alexander <ashishalex29@gmail.com>
2024-10-16 20:46:21 +0530

    admin signIn added with jwt

--- src/repositories/interfaces/user.ts
+++ src/repositories/interfaces/user.ts
@@ -1,6 +1,5 @@
 // src/repositories/interfaces/user.repository.interface.ts
 import { IUser } from '../../models/userModel';
-
 export interface IUserRepository {
   findUserByEmail(email: string): Promise<IUser | null>;
   createUser(userData: Partial<IUser>): Promise<IUser>;
