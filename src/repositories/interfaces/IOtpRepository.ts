60b9f99387a5c12c4dc943cee955b46f0bb3d7fa
Ashish Alexander <ashishalex29@gmail.com>
2024-10-31 10:33:50 +0530

    bug fix

--- src/repositories/interfaces/IOtpRepository.ts
+++ src/repositories/interfaces/IOtpRepository.ts
@@ -3,4 +3,5 @@ import {IOtp} from '../../models/otpModel'
 export interface IOtpRepository {
     createOtpEntry(email: string, otp: string, expiresAt: Date): Promise<IOtp>;
     findOtpByEmail(email: string): Promise<IOtp | null>;
+    deleteOtpById(id:unknown): Promise<void>
 }
